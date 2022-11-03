dfb25c3604393854464c39855fa5e0939afa1fde
DevLuv_JinHyung <wlsgud16@gmail.com>
2022-11-04 03:50:29 +0900

    2022/11/04 Update) Silver3
    Update)
    -Silver3: 1003번 (dp 알고리즘 연습)

---
+++ Solvedac & Baekjoon/Silver3/1003.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+
+int main(void)
+{
+	//피보나치 dp 알고리즘 응용
+	const int MAX_SIZE = 40; //배열 최대 사이즈
+	int testCase = 0;
+
+	std::vector<int> dpFibonacci(MAX_SIZE + 1);
+	dpFibonacci[0] = 0;
+	dpFibonacci[1] = 1;
+
+	for (int f = 2; f < dpFibonacci.size(); f++)
+	{
+		dpFibonacci[f] = dpFibonacci[f - 1] + dpFibonacci[f - 2];
+	}
+
+	std::cin >> testCase;
+	for (int i = 0; i < testCase; i++)
+	{
+		int n = 0;
+		std::cin >> n;
+
+		if (n == 0)
+		{
+			std::cout << "1 0" << std::endl;
+		}
+		else
+		{
+			//0출력 횟수는 n이 0일때 1, 1출력 횟수는 n이 1일때 부터
+			std::cout << dpFibonacci[n - 1] << " " << dpFibonacci[n] << std::endl;
+		}
+	}
+	return 0;
+}
