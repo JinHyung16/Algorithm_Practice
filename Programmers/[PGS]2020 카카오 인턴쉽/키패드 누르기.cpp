3e44d3a01fa4a307a880fec807683f8572b1cc31
DevLuv_JinHyung <wlsgud16@gmail.com>
2022-11-19 12:01:17 +0900

    2022/11/19 Update) [PSG]기출 문제
    Update)
    -[PGS]기출 문제: 기사단원의 무기
    
    Modify)
    -PSG -> PGS로 프로그래머스 약자 잘못 쓴 거 수정

--- Programmers/[PGS]2020 카카오 인턴쉽/키패드 누르기.cpp
+++ Programmers/[PGS]2020 카카오 인턴쉽/키패드 누르기.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 string solution(vector<int> numbers, string hand) {
     string answer = "";
-    int leftHand = 10, leftDis = 0; // *은 10로 대체
-    int rightHand = 12, rightDis = 0; // #은 12로 대체
+
+    int leftHand = 10, leftDis = 0; //*은 10로 대체
+    int rightHand = 12, rightDis = 0; //#은 12로 대체
     for (int i = 0; i < numbers.size(); i++)
     {
         if (numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7)
@@ -20,9 +21,9 @@ string solution(vector<int> numbers, string hand) {
             answer += "R";
             rightHand = numbers[i];
         }
-        else // 2, 5, 8, 0 중 하나 누른 경우
+        else //2, 5, 8, 0 중 하나 누른 경우
         {
-            if (numbers[i] == 0) // 0은 11로 대체
+            if (numbers[i] == 0) //0은 11로 대체
             {
                 numbers[i] = 11;
             }
@@ -51,7 +52,7 @@ string solution(vector<int> numbers, string hand) {
                 answer += "L";
                 leftHand = numbers[i];
             }
-            else // rightDis < leftDis의 경우
+            else //rightDis < leftDis의 경우
             {
                 answer += "R";
                 rightHand = numbers[i];
