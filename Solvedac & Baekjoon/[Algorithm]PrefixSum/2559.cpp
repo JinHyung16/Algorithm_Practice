b7f12fc5df049b02fc59d666e77a41d8f5cc3e10
DevLuv_JinHyung <wlsgud16@gmail.com>
2023-08-22 21:54:32 +0900

    2023/08/22 Modify) [Algorithm]PrefixSum
    Modify)
    -[Algorithm]PrefixSum: 2559번

--- Solvedac & Baekjoon/[Algorithm]PrefixSum/2559.cpp
+++ Solvedac & Baekjoon/[Algorithm]PrefixSum/2559.cpp
@@ -8,6 +8,11 @@
 /// Silver3 수열 - 누적합 알고리즘
 /// 누적합을 위해, prefix[i]의 이전 값을 더해서 계속 넣어둔다.
 /// 그 후 i = K to N해서 result의 prefix[i] - prefix[i-K]값을 넣는다.
+/// 
+/// 08월21일 문제만 풀고 채점을 안했다.
+/// 08월22일 채점 결과 틀렸다... 그래서 다시 도전해서 풀고 올렸다.
+/// 틀린 이유는 result를 MIN으로 초기화 안해서 틀렸다. 
+/// 왜 초기화 해야하냐면 음수값이 나올 수 있기 때문이다.
 /// </summary>
 
 using namespace std;
@@ -19,7 +24,7 @@ int main(void)
 	int N = 0, K = 0; //온도를 측정한 전체 날짜 N, 합을 구하기 위한 연속적인 날짜 K
 	std::cin >> N >> K;
 	vector<int> prefix(N + 1, 0);
-	int result = 0;
+	int result = -987654321; //다 음수일수 있으니 생각해서 초기화하자
 
 	for ( int i = 1; i <= N; i++ )
 	{
