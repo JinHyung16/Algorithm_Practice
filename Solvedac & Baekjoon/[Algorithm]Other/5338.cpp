f69a9e519aea92492ecff5cffd2abd45bd0a27e0
Dev_JinHyung <wlsgud16@gmail.com>
2024-01-02 21:12:24 +0900

    2024/01/02 Update) [Algorithm]Other
    Update)
    -[Algorithm]Other: 5338번

---
+++ Solvedac & Baekjoon/[Algorithm]Other/5338.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+#define FastIO ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+
+using namespace std;
+
+/// <summary>
+/// Bronze5 마이크로소프트 로고 - 구현 알고리즘
+/// 한줄 한줄 그대로 출력하면 된다.
+/// </summary>
+
+
+int main(void)
+{
+    FastIO;
+
+    cout << "       _.-;;-._" << "\n";
+    cout << "'-..-'|   ||   |" << "\n";
+    cout << "'-..-'|_.-;;-._|" << "\n";
+    cout << "'-..-'|   ||   |" << "\n";
+    cout << "'-..-'|_.-''-._|" << "\n";
+
+    return 0;
+}
