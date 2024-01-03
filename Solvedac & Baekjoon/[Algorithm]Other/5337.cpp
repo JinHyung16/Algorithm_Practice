20fc0821a70cc84085f1379171d863f7fcb487df
Dev_JinHyung <wlsgud16@gmail.com>
2024-01-03 20:37:18 +0900

    2024/01/03 Update) [Algorithm]Other
    Update)
    -[Algorithm]Other: 5337번

---
+++ Solvedac & Baekjoon/[Algorithm]Other/5337.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+#define FastIO ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+
+using namespace std;
+
+/// <summary>
+/// Bronze5 웰컴 - 구현 알고리즘
+/// 한줄 한줄 그대로 출력하면 된다.
+/// </summary>
+
+
+int main(void)
+{
+    FastIO;
+
+    cout << ".  .   ." << "\n";
+    cout << "|  | _ | _. _ ._ _  _" << "\n";
+    cout << "|/\\|(/.|(_.(_)[ | )(/." << "\n";
+
+    return 0;
+}
