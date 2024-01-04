a09747d8494ec71934221ea8c530840c0269ffe5
Dev_JinHyung <wlsgud16@gmail.com>
2024-01-04 20:58:13 +0900

    2024/01/04 Update) [Algorithm]Other
    Update)
    -[Algorithm]Other: 9653번

---
+++ Solvedac & Baekjoon/[Algorithm]Other/9653.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+#define FastIO ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+
+using namespace std;
+
+/// <summary>
+/// Bronze5 스타워즈 로고 - 구현 알고리즘
+/// 한줄 한줄 그대로 출력하면 된다.
+/// </summary>
+
+
+int main(void)
+{
+    FastIO;
+
+    cout << "    8888888888  888    88888" << "\n";
+    cout << "   88     88   88 88   88  88" << "\n";
+    cout << "    8888  88  88   88  88888" << "\n";
+    cout << "       88 88 888888888 88   88" << "\n";
+    cout << "88888888  88 88     88 88    888888" << "\n";
+    cout << "\n";
+    cout << "88  88  88   888    88888    888888" << "\n";
+    cout << "88  88  88  88 88   88  88  88" << "\n";
+    cout << "88 8888 88 88   88  88888    8888" << "\n";
+    cout << " 888  888 888888888 88  88      88" << "\n";
+    cout << "  88  88  88     88 88   88888888" << "\n";
+
+    return 0;
+}
