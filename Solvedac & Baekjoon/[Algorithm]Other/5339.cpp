e0bfaec1f15b04b46d9b0de70e49c6e3a1318c56
Dev_JinHyung <wlsgud16@gmail.com>
2024-01-06 02:03:48 +0900

    2024/01/05 Update) [Algorithm]Other
    Update)
    -[Algorithm]Other: 5339번

---
+++ Solvedac & Baekjoon/[Algorithm]Other/5339.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+#define FastIO ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+
+using namespace std;
+
+/// <summary>
+/// Bronze5 콜센터 - 구현 알고리즘
+/// 한줄 한줄 그대로 출력하면 된다.
+/// </summary>
+
+
+int main(void)
+{
+    FastIO;
+
+	cout << "     /~\\" << '\n';
+	cout << "    ( oo|" << '\n';
+	cout << "    _\\=/_" << '\n';
+	cout << "   /  _  \\" << '\n';
+	cout << "  //|/.\\|\\\\ " << '\n';
+	cout << " ||  \\ /  ||" << '\n';
+	cout << "============" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+
+    return 0;
+}
