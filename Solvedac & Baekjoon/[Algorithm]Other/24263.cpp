372a909a60d843b68fae993546b71a0b81ce3c23
DevLuv_JinHyung <wlsgud16@gmail.com>
2024-01-20 23:57:20 +0900

    2024/01/20 Update) [Algorithm]Other
    Update)
    -[Algorithm]Other: 24263번

---
+++ Solvedac & Baekjoon/[Algorithm]Other/24263.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+#define FastIO ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+
+using namespace std;
+
+/// <summary>
+/// Bronze4 알고리즘 수업 - 알고리즘의 수행 시간 2 - 구현 알고리즘
+/// 1차원 배열의 시간 복잡도는 O(N)으로 표시할 떄
+/// 결국 최고차항은 항상 1이고 N만큼 선회한다.
+/// </summary>
+
+
+int main(void)
+{
+	FastIO;
+
+	int N = 0; // 배열 수행횟수 N
+	cin >> N;
+	cout << N << "\n" << "1" << "\n";
+	return 0;
+}
