c41681066d126c23a9c3a21ab8007826ea25a9cb
DevLuv_JinHyung <wlsgud16@gmail.com>
2024-02-07 20:43:43 +0900

    2024/02/07 Update) [Algorithm]Other
    Update)
    -[Algorithm]Other: 25238번

---
+++ Solvedac & Baekjoon/[Algorithm]Other/25238.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<vector>
+
+#define FastIO ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+
+using namespace std;
+
+/// <summary>
+/// Bronze4 가희와 방어율 무시 - 구현 알고리즘
+/// </summary>
+
+
+int main(void)
+{
+	FastIO;
+
+	int a = 0, b = 0; // 몬스터 방어율 a, 유저 방무 b
+	cin >> a >> b;
+
+	double defend = a - a * (b / 100.0);
+	
+	if ( 100 <= defend )
+		cout << 0 << "\n";
+	else
+		cout << 1 << "\n";
+
+	return 0;
+}
