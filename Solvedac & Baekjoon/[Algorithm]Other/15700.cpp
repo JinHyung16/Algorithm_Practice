f5ba5d676476040900967c081c2a7e9091bb00bd
DevLuv_JinHyung <wlsgud16@gmail.com>
2024-02-07 20:35:16 +0900

    2024/02/06 Update) [Algorithm]Other
    Update)
    -[Algorithm]Other: 15700번

---
+++ Solvedac & Baekjoon/[Algorithm]Other/15700.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<vector>
+
+#define FastIO ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+
+using namespace std;
+
+/// <summary>
+/// Bronze4 타일 채우기 4 - 구현 알고리즘
+/// </summary>
+
+
+int main(void)
+{
+	FastIO;
+
+	long long N = 0, M = 0; // N * M 크기 벽
+	cin >> N >> M;
+
+	cout << (N * M) / 2 << "\n";
+
+	return 0;
+}
